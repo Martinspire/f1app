ff3929082d54f40fe819b825e3c3a1097c280573
Martin Spierings <martin@martinspire.nl>
2023-02-23 17:26:14 +0100

    Github Pages release
    Github pages deploy toevoegen
    Items updaten zodat ie niet http pakt (hopelijk)

--- apps/formule1/src/app/constants/app.constants.ts
+++ apps/formule1/src/app/constants/app.constants.ts
@@ -1,10 +1,10 @@
 export const AppConstant = {
-  apiURL: 'http://ergast.com/api/f1/',
-  mapProvider: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  apiURL: '//ergast.com/api/f1/',
+  mapProvider: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 
   // dont forget &origin=* to get around CORS
-  wikiImageUrl: 'https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&origin=*&format=json&pithumbsize=400&titles=',
-  wikiSummaryUrl: 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&origin=*&exintro&explaintext&redirects=1&titles=',
+  wikiImageUrl: '//en.wikipedia.org/w/api.php?action=query&prop=pageimages&origin=*&format=json&pithumbsize=400&titles=',
+  wikiSummaryUrl: '//en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&origin=*&exintro&explaintext&redirects=1&titles=',
   songs: [
     {
       id: 'f1-theme',

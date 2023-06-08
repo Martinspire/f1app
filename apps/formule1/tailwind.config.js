98b478288f5ada66e59b722f1e4febe69aa23b14
Martin Spierings <martin@martinspire.nl>
2023-06-08 18:41:34 +0200

    Migrate angular 16

--- apps/formule1/tailwind.config.js
+++ apps/formule1/tailwind.config.js
@@ -1,31 +1,33 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 
-const daisyui = require("daisyui");
+const daisyui = require('daisyui');
 const tailwindAspectRatio = require('@tailwindcss/aspect-ratio');
 // this is disabled because it currently gives a few autoprefixer warnings and we don't need it yet
 // const tailwindForms = require('@tailwindcss/forms');
-const tailwindLineClamp = require('@tailwindcss/line-clamp');
-const tailwindTypography = require("@tailwindcss/typography");
+const tailwindTypography = require('@tailwindcss/typography');
 
 module.exports = {
-    content: [
-        join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-        ...createGlobPatternsForDependencies(__dirname),
-    ],
-    theme: {
-        extend: {},
-        fontFamily: {
-          sans: ['"Formula1-Regular"', '"Helvetica Neue"', 'Arial', 'sans-serif'],
-          heading: ['"Formula1-Black"', '"Helvetica Neue"', 'Arial', 'sans-serif'],
-          special: ['"Formula1-Wide"', '"Helvetica Neue"', 'Arial', 'sans-serif']
-        }
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['"Formula1-Regular"', '"Helvetica Neue"', 'Arial', 'sans-serif'],
+      heading: ['"Formula1-Black"', '"Helvetica Neue"', 'Arial', 'sans-serif'],
+      special: ['"Formula1-Wide"', '"Helvetica Neue"', 'Arial', 'sans-serif'],
     },
-    plugins: [
-        tailwindAspectRatio,
-        tailwindLineClamp,
-        // tailwindForms,
-        tailwindTypography,
-        daisyui
-    ],
+  },
+  plugins: [
+    tailwindAspectRatio,
+    // tailwindForms,
+    tailwindTypography,
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["light", "dark",],
+    logs: false // no version debugging needed
+  }
 };
